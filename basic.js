3f8c24882a3152b9480c0e5d526a8533e8ff1378
swarnabha <swarnabhadutta909@gmail.com>
2026-05-21 13:50:35 +0530

    learning typescript

---
+++ basic.js
@@ -0,0 +1,7 @@
+"use strict";
+function greet2(name) {
+    return `Hello , ${name}`;
+}
+console.log(greet2("hitesh"));
+// console.log(greet2(true));
+// console.log(greet2(43));
---
+++ basic.ts
@@ -0,0 +1,9 @@
+function greet2(name : string) :string{
+    return `Hello , ${name}`;
+}
+
+
+console.log(greet2("hitesh"));
+// console.log(greet2(true));
+// console.log(greet2(43));
+ 
